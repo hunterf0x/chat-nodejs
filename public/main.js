c9666d3fb409adea723db2ab3111dda273bfcbcb
hvaras <king.cold@gmail.com>
2016-01-04 18:32:28 -0300

    commit inicial

---
+++ public/main.js
@@ -0,0 +1,31 @@
+var socket = io.connect('http://192.168.1.87:3000', { 'forceNew': true });
+
+socket.on('messages', function(data) {
+  console.log(data);
+  render(data);
+})
+
+function render (data) {
+  var html = data.map(function(elem, index) {
+
+    return(`<div>
+              <strong>${elem.author}</strong>:
+              <em>${elem.text}</em>
+            </div>`);
+  }).join(" ");
+
+  document.getElementById('messages').innerHTML = html;
+}
+
+function addMessage(e) {
+  var message = {
+    author: document.getElementById('username').value,
+    text: document.getElementById('texto').value
+  };
+
+  socket.emit('new-message', message);
+  document.getElementById('texto').value = '';
+  document.getElementById('texto').focus();
+
+  return false;
+}
---
+++ server/main.js
@@ -0,0 +1,31 @@
+var express = require('express');  
+var app = express();  
+var server = require('http').Server(app);  
+var io = require('socket.io')(server);
+
+var messages = [{  
+  id: 1,
+  text: "Escribe algo ctm",
+  author: "sh0riz0"
+}];
+
+app.use(express.static('../public'));
+
+app.get('/hello', function(req, res) {  
+  res.status(200).send("Hello World!");
+});
+
+io.on('connection', function(socket) {  
+  console.log('Alguien se ha conectado con Sockets');
+  socket.emit('messages', messages);
+
+  socket.on('new-message', function(data) {
+    messages.push(data);
+
+    io.sockets.emit('messages', messages);
+  });
+});
+
+server.listen(3000, function() {  
+  console.log("Servidor corriendo en http://localhost:8080");
+});
