a33b2a9f95c2e19b718ee944ad1a5fb96205d8d6
hvaras <king.cold@gmail.com>
2016-01-05 10:24:28 -0300

    se cambia la ip de mi red LAN  por la de localhost

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://192.168.1.87:3000', { 'forceNew': true });
+var socket = io.connect('http://localhost:3000', { 'forceNew': true });
 
 socket.on('messages', function(data) {
   console.log(data);
